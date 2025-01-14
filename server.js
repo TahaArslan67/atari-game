d37aae0733793bf87870f47d30aeb16d5876efe1
MONSTER <your-email@example.com>
2025-01-14 03:37:40 +0300

    CommonJS formatına geçildi

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import cors from 'cors';
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = createServer(app);
